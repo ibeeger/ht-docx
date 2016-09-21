b5c41edcb10ef9b964f6f20d788d264759c99c7b
ibeeger <cxh80301983@126.com>
2016-09-21 18:44:03 +0800

    up

--- test/demo.js
+++ test/demo.js
@@ -2,7 +2,7 @@
 * @Author: willclass
 * @Date:   2016-08-31 10:21:57
 * @Last Modified by:   ibeeger
-* @Last Modified time: 2016-09-09 14:47:30
+* @Last Modified time: 2016-09-19 14:47:41
 */
 
 'use strict';
@@ -15,13 +15,13 @@ var file = process.argv[2];
 
 var Item = new Docx(file,{
 	pandoc:"/usr/local/bin/pandoc",
-	cols:"2",
+	cols:"3",
 	format:"A3",
 	// orientation: "portrait",
 });
 
-Item.toFile(function(){
-	console.log(arguments)
+Item.toFile("ddd.docx",function(){
+	console.log(arguments);
 });
 
 
