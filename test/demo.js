e793aff26b10b9e7bc80a76907c4b5672d3d7452
ibeeger <cxh80301983@126.com>
2016-10-14 14:34:59 +0800

    up

--- test/demo.js
+++ test/demo.js
@@ -2,7 +2,7 @@
 * @Author: willclass
 * @Date:   2016-08-31 10:21:57
 * @Last Modified by:   ibeeger
-* @Last Modified time: 2016-09-19 14:47:41
+* @Last Modified time: 2016-10-14 14:31:38
 */
 
 'use strict';
@@ -15,7 +15,7 @@ var file = process.argv[2];
 
 var Item = new Docx(file,{
 	pandoc:"/usr/local/bin/pandoc",
-	cols:"3",
+	cols:"2",
 	format:"A3",
 	// orientation: "portrait",
 });
